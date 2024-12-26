abd311d63f94e66d158e33c6ffb8eac6e329be94
Anas Zenagui <zenaguianas20@gmail.com>
2024-12-26 21:26:29 +0100

    Update

--- components/tables/user-tables/entreprises.tsx
+++ components/tables/user-tables/entreprises.tsx
@@ -1,26 +1,28 @@
 "use client";
-import { DataTable } from "@/components/ui/data-table";
-import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { columns } from "@/components/tables/request-tables/columns";
+import { columns } from "@/components/tables/employee-tables/columns";
 import { FC } from "react";
+import { EntrepriseDataTable } from "@/components/ui/entreprise-table";
+import { Heading } from "@/components/ui/heading";
 import { EnterpriseData } from "@/types";
+import { DataTable } from "@/components/ui/data-table";
 
 interface EntrepriseProps {
   data: EnterpriseData[];
 }
 
 export const UserEnterprise: FC<EntrepriseProps> = ({ data }) => {
+
   return (
     <>
       <div className="flex items-start justify-between">
         <Heading
-          title={`Requests (${data.filter(entreprise => entreprise.is_verified == "Pending").length})`}
-          description="Validating the entreprise requests"
+          title={`Enterprises (${data.filter(entreprise => entreprise.is_verified == "Accepted").length})`}
+          description="Management des enterprises"
         />
       </div>
       <Separator />
-      <DataTable searchKey="company_name" columns={columns} data={data} />
+      <DataTable searchKey="company_name" columns={columns} data={data.filter(entreprise => entreprise.is_verified == "Accepted")} />
     </>
   );
 };
