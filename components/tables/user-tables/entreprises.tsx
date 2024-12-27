0ec6fd69b5e9bda6373ac6a65e3fe97ae477a0ae
Anas Zenagui <zenaguianas20@gmail.com>
2024-12-27 12:10:43 +0100

    Update

--- components/tables/user-tables/entreprises.tsx
+++ components/tables/user-tables/entreprises.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { DataTable } from "@/components/ui/data-table";
+// import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { columns } from "@/components/tables/request-tables/columns";
 import { FC } from "react";
 import { EnterpriseData } from "@/types";
+import { EntrepriseDataTable } from "@/components/ui/entreprise-table";
 
 interface EntrepriseProps {
   data: EnterpriseData[];
@@ -20,7 +21,7 @@ export const UserEnterprise: FC<EntrepriseProps> = ({ data }) => {
         />
       </div>
       <Separator />
-      <DataTable searchKey="company_name" columns={columns} data={data} />
+      <EntrepriseDataTable searchKey="company_name" columns={columns} data={data} />
     </>
   );
 };
